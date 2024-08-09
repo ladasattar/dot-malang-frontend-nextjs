ba06c9b5e569b4ec0aef5a7cba4d2ca76d374c28
Muqorroba Lada Sattar <ladasattar@gmail.com>
2024-08-09 07:10:14 +0700

    feat: add navbar & stepper bar

---
+++ components/StepperBar/index.tsx
@@ -0,0 +1,8 @@
+export default function StepperBar() {
+  return (
+    <section className="bg-white flex gap-2 items-center justify-center h-[90px] drop-shadow-md">
+      <div className="bg-brand rounded-full w-2.5 h-2.5"></div>
+      <p className="font-bold">SHIPPING DETAILS</p>
+    </section>
+  );
+}
